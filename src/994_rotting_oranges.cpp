2d3937304f64d3e73c27cd183f92cf30ef1cb68c
Conner Rose <conner.n.rose@gmail.com>
2024-05-05 13:19:11 -0400

    Finish rotting oranges

---
+++ src/994_rotting_oranges.cpp
@@ -0,0 +1,43 @@
+#include <queue>
+#include <utility>
+#include <vector>
+
+using std::pair;
+using std::queue;
+using std::vector;
+
+class Solution {
+ public:
+  int orangesRotting(vector<vector<int>>& grid) {
+    int m = grid.size(), n = grid[0].size(), fresh = 0;
+    queue<pair<int, int>> q;
+    for (int i = 0; i < m; ++i) {
+      for (int j = 0; j < n; ++j) {
+        if (grid[i][j] == 2) {
+          q.push({i, j});
+        } else if (grid[i][j] == 1) {
+          ++fresh;
+        }
+      }
+    }
+    int res = 0, dir[] = {0, 1, 0, -1, 0};
+    while (!q.empty() && fresh > 0) {
+      ++res;
+      int size = q.size();
+      while (size--) {
+        auto [i, j] = q.front();
+        q.pop();
+        for (int k = 0; k < 4; ++k) {
+          int r = i + dir[k];
+          int c = j + dir[k + 1];
+          if (r >= 0 && r < m && c >= 0 && c < n && grid[r][c] == 1) {
+            --fresh;
+            grid[r][c] = 2;
+            q.push({r, c});
+          }
+        }
+      }
+    }
+    return fresh == 0 ? res : -1;
+  }
+};
