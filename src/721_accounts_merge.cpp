256da4d8635912e60812059ae463235c16720a05
Conner Rose <conner.n.rose@gmail.com>
2024-05-06 15:24:44 -0400

    Finish accounts merge

---
+++ src/721_accounts_merge.cpp
@@ -0,0 +1,62 @@
+#include <algorithm>
+#include <string>
+#include <unordered_map>
+#include <vector>
+
+using std::sort;
+using std::string;
+using std::unordered_map;
+using std::vector;
+
+class Solution {
+  // Find parent with path compression
+  int find(int parent, vector<int>& parents) {
+    while (parent != parents[parent]) {
+      parent = parents[parent];
+    }
+    return parent;
+  }
+
+ public:
+  vector<vector<string>> accountsMerge(vector<vector<string>>& accounts) {
+    vector<int> parents(accounts.size(), 0);
+    unordered_map<string, int> email_to_account;
+
+    for (int account = 0; account < accounts.size(); ++account) {
+      // Every element is initially its own parent
+      parents[account] = account;
+      for (int j = 1; j < accounts[account].size(); ++j) {
+        const string& email = accounts[account][j];
+        auto iter = email_to_account.find(email);
+        if (iter != email_to_account.end()) {
+          // If we've already encountered this email,
+          // set this email's ancestor account to the current account
+          parents[find(iter->second, parents)] = account;
+        }
+        email_to_account[email] = account;
+      }
+    }
+
+    unordered_map<int, int> account_to_idx;
+    vector<vector<string>> res;
+    for (const auto& [email, account] : email_to_account) {
+      int parent = find(account, parents);
+      auto iter = account_to_idx.find(parent);
+      int idx;
+      if (iter != account_to_idx.end()) {
+        idx = iter->second;
+      } else {
+        idx = res.size();
+        account_to_idx[parent] = idx;
+        res.emplace_back(1, accounts[account][0]);
+      }
+      res[idx].push_back(email);
+    }
+
+    for (vector<string>& emails : res) {
+      sort(emails.begin() + 1, emails.end());
+    }
+
+    return res;
+  }
+};
