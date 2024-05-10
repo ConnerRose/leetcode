bb83220f2e5b68fcbcf9d9d4cc3fbd5b4ef2a347
Conner Rose <conner.n.rose@gmail.com>
2024-05-10 07:24:59 -0400

    Finish partition sum

---
+++ src/416_partition_sum.cpp
@@ -0,0 +1,25 @@
+#include <bitset>
+#include <numeric>
+#include <vector>
+
+using std::accumulate;
+using std::bitset;
+using std::vector;
+
+class Solution {
+ public:
+  bool canPartition(vector<int>& nums) {
+    int sum = accumulate(nums.begin(), nums.end(), 0);
+    if (sum % 2 == 1) {
+      return false;
+    }
+    int half = sum / 2;
+    // Each bit represents whether or not that sum can be reached
+    // 10001 is maximum possible sum / 2
+    std::bitset<10001> dp(1);
+    for (int num : nums) {
+      dp |= dp << num;
+    }
+    return dp[half];
+  }
+};
