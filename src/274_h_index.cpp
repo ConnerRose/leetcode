4dd23fc7c418d49f70d9526916bdeae29375d1cf
Conner Rose <conner.n.rose@gmail.com>
2024-07-23 19:13:52 -0400

    Finish h index

---
+++ src/274_h_index.cpp
@@ -0,0 +1,18 @@
+#include <algorithm>
+#include <map>
+#include <vector>
+
+using namespace std;
+
+class Solution {
+ public:
+  auto hIndex(vector<int>& citations) -> int {
+    int n = static_cast<int>(citations.size());
+    sort(citations.begin(), citations.end());
+    int res = 0;
+    for (int i = 0; i < n; ++i) {
+      res = max(res, min(n - i + 1, citations[i]));
+    }
+    return res;
+  }
+};
